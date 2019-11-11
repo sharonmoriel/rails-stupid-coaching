d8c5dc8fff4f3ca5c84c207f7c06eff177cacc3f
Sharon Moriel <shmoriel@gmail.com>
2019-11-11 15:22:47 +0000

    add form to ask questions and get answer from the coach

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+  # home
+  root to: 'questions#ask'
+  # questions routes
+  get 'ask', to: 'questions#ask'
+  get 'answer', to: 'questions#answer'
+
+  post 'ask', to: 'questions#answer'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
